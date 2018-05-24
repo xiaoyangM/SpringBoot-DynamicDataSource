4b281f8df3c714e2d9ec978daf82fcaf05d63949
ylsh <xiaoyang_mo@suhui.com>
2018-05-24 11:06:04 +0800

    18-05-24 代码优化

--- src/main/java/com/dynamicdb/config/DataSourceConfigurer.java
+++ src/main/java/com/dynamicdb/config/DataSourceConfigurer.java
@@ -2,7 +2,6 @@ package com.dynamicdb.config;
 
 import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceBuilder;
 import com.dynamicdb.common.DataSourceKey;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
